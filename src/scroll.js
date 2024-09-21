895116c0522b8843ef72401edc3b6152c0e57100
Nans Marchis-Mouren <nans.marchis-mouren@dualsun.com>
2024-09-21 10:02:40 +0200

    init

---
+++ src/scroll.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+const logo = document.querySelector("header svg");
+
+const scrollObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        header.classList.remove("bg-transparent");
+        header.classList.add("bg-white");
+        header.classList.add("dark:bg-gray-800");
+        header.classList.add("shadow-md");
+        logo.classList.remove("fill-gray-800");
+        logo.classList.remove("dark:fill-white");
+        logo.classList.add("fill-red-500");
+      } else {
+        header.classList.remove("bg-white");
+        header.classList.remove("dark:bg-gray-800");
+        header.classList.add("bg-transparent");
+        header.classList.remove("shadow-md");
+        logo.classList.remove("fill-red-500");
+        logo.classList.add("fill-gray-800");
+        logo.classList.add("dark:fill-white");
+      }
+    });
+  },
+  { rootMargin: "-4px" },
+);
+
+scrollObserver.observe(document.querySelector("#scroll"));
